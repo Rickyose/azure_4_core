68d7e6abc1342cd5585b18b571f2e120fc5aaff9
Rickyose <rickyhaterearged@gmail.com>
2022-03-02 03:20:24 +0700

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -f sudah_initiate_raptoreum.txt ]; then
+  touch sudah_initiate_raptoreum.txt
+  sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git -y
+  git clone https://github.com/WyvernTKC/cpuminer-gr-avx2
+  cd cpuminer-gr-avx2
+  ./build.sh
+  wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config
+else
+fi
+
+sleep 60
+
+cd
+cd /home/ubuntu/cpuminer-gr-avx2
+
+sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RU9x5mebSSmeuaZ2HjEACQAMJX3Ajs6HzF -p xxxxxx
+
+# sudo ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RYKqtRnxDFXx8VeVSSGxn2g3ghW7dxJm6q.Rig001 -p x --cpu 2
