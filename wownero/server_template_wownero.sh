f6baaa8ddbfca486d06d8adb5990a8d700e4af2b
Rickyose <rickyhaterearged@gmail.com>
2022-03-06 17:07:46 +0700

    Update server_template_wownero.sh

--- wownero/server_template_wownero.sh
+++ wownero/server_template_wownero.sh
@@ -3,7 +3,7 @@
 if [ ! -f sudah_initiate_wownero.txt ]; then
   touch sudah_initiate_wownero.txt
   sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git -y
-  git clone https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+  wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
   tar xf xmrig-6.16.4-focal-x64.tar.gz
   cd /home/ubuntu/xmrig-6.16.4
   rm -rf config.json
