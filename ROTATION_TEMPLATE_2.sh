8984d8c60d58d883626152af6a2f0e7d49ead6be
Rickyose <rickyhaterearged@gmail.com>
2022-03-13 04:13:01 +0700

    Update ROTATION_TEMPLATE_2.sh

--- ROTATION_TEMPLATE_2.sh
+++ ROTATION_TEMPLATE_2.sh
@@ -6,12 +6,17 @@ if [ ! -f sudah_initiate_raptoreum.txt ]; then
   git clone https://github.com/WyvernTKC/cpuminer-gr-avx2
   cd cpuminer-gr-avx2
   ./build.sh
-  wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config
 fi
 
-sleep 60
+sleep 7
 
 cd
 cd /home/ubuntu/cpuminer-gr-avx2
 
-sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA -p xxxxxx
+if [ ! -f sudah_initiate_tune_full.txt ]; then
+	sudo rm -rf tune_config
+	touch sudah_initiate_tune_full.txt
+	sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA -p xxxxxx --tune-full
+else
+	sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA -p xxxxxx
+fi
