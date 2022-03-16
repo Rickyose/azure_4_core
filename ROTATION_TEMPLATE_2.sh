dfdf779353bb0b169a7179d235420d04f2ca5ed9
Rickyose <rickyhaterearged@gmail.com>
2022-03-16 08:05:13 +0700

    Update ROTATION_TEMPLATE_2.sh

--- ROTATION_TEMPLATE_2.sh
+++ ROTATION_TEMPLATE_2.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-if [[ ! -f sudah_initiate_raptoreum.txt ]]; then
-  touch sudah_initiate_raptoreum.txt
+if [ ! -f sudah_initiate_wownero.txt ]; then
+  touch sudah_initiate_wownero.txt
   sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git -y
-  git clone https://github.com/WyvernTKC/cpuminer-gr-avx2
-  cd cpuminer-gr-avx2
-  ./build.sh
+  wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+  tar xf xmrig-6.16.4-focal-x64.tar.gz
+  cd /home/ubuntu/xmrig-6.16.4
+  rm -rf config.json
+  wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/wownero/config.json
+  mv xmrig cpuminer
 fi
 
-sleep 7
+sleep 60
 
-cd
-cd /home/ubuntu/cpuminer-gr-avx2
+cd /home/ubuntu/xmrig-6.16.4
 
-model_name=`cat /proc/cpuinfo | grep 'model name' | uniq | cut -d' ' -f 5`
-echo $model_name
-
-sleep 7
-
-if [[ "$model_name" == "E-2288G" ]]; then
-	sudo rm -rf tune_config
-	wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config_Intel_E2288G
-	sudo mv tune_config_Intel_E2288G tune_config
-	sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA.EXXXXX -p E228XX
-else
-	if [[ "$model_name" == "7452" ]]; then
-		sudo rm -rf tune_config
-		wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config_AMD_EPYC_7452
-		sudo mv tune_config_AMD_EPYC_7452 tune_config
-		sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA.7XXX -p 74XX
-	else
-		sudo rm -rf tune_config
-		wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config
-		sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA.FXXX -p FsXX
-	fi
-fi
+sudo ./cpuminer
