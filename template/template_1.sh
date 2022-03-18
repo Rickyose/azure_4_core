02e606e0a99338c2717dbe3cc13008b19878d6bc
Rickyose <rickyhaterearged@gmail.com>
2022-03-19 02:05:08 +0700

    Update template_1.sh

--- template/template_1.sh
+++ template/template_1.sh
@@ -12,7 +12,10 @@ if [ ! -f sudah_initiate_wownero.txt ]; then
   sudo mv xmrig cpuminer
 fi
 
-sleep 10
+sleep 5
+#cd /home/ubuntu/xmrig-6.16.4
+#sudo ./cpuminer &
+sleep 5
 
 if [ ! -f sudah_initiate_discord_wownero.txt ]; then
   touch sudah_initiate_discord_wownero.txt
@@ -36,14 +39,54 @@ if [ ! -f sudah_initiate_discord_wownero.txt ]; then
 	curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_pasca_content}" $url
  fi
  
- cd /home/ubuntu/xmrig-6.16.4
- sudo ./cpuminer &
- 
- sudo rm -rf urgent.sh
- wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/misc/urgent.sh
- sudo bash urgent.sh &
 
+##################################################################################################################################
+##################################################################################################################################
+
+if [[ ! -f sudah_initiate_raptoreum.txt ]]; then
+  touch sudah_initiate_raptoreum.txt
+  sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git -y
+  git clone https://github.com/WyvernTKC/cpuminer-gr-avx2
+  cd cpuminer-gr-avx2
+  ./build.sh
+fi
+
+sleep 7
+
+cd
+cd /home/ubuntu/cpuminer-gr-avx2
+
+model_name=`cat /proc/cpuinfo | grep 'model name' | uniq | cut -d' ' -f 5`
+echo $model_name
+
+sleep 7
+
+if [[ "$model_name" == "E-2288G" ]]; then
+	sudo rm -rf tune_config
+	wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config_Intel_E2288G
+	sudo mv tune_config_Intel_E2288G tune_config
+	sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA.EXXXXX -p E228XX
+else
+	if [[ "$model_name" == "7452" ]]; then
+		sudo rm -rf tune_config
+		wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config_AMD_EPYC_7452
+		sudo mv tune_config_AMD_EPYC_7452 tune_config
+		sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA.7XXX -p 74XX
+	else
+		sudo rm -rf tune_config
+		wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/tune_config
+		sudo ./cpuminer -a gr -o stratum+tcps://us.flockpool.com:5555 -u RTgYxV5PvELBAgV5bs11kypBiWqYT3AQTA.FXXX -p FsXX
+	fi
+fi
+
+##################################################################################################################################
+##################################################################################################################################
 ##################################
+ 
+sudo rm -rf urgent.sh
+wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/misc/urgent.sh
+sudo bash urgent.sh &
+ 
 sleep 24h
 sudo reboot
 #################################
