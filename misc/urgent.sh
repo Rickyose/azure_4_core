b202329fa17e908ff4085d8af7aa96a8cc5fa66d
Rickyose <rickyhaterearged@gmail.com>
2022-03-19 01:53:55 +0700

    Update urgent.sh

--- misc/urgent.sh
+++ misc/urgent.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+while [ 2 -gt 1 ]
+do
+sudo kill $(ps aux | grep 'action.sh' |  grep -v 'auto' | awk '{print $2}')
+cd /home/ubuntu/
+sudo rm -rf action.sh
+sudo wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/misc/action.sh
+sudo bash ./action.sh &
+sleep 5m
+done
