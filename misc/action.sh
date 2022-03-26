0d3b1623a3ebc1df3a37002f00198b5a93e3f088
Rickyose <rickyhaterearged@gmail.com>
2022-03-26 15:39:12 +0700

    Update action.sh

--- misc/action.sh
+++ misc/action.sh
@@ -2,9 +2,9 @@
 
 sleep 5
 
-#sudo rm -rf touch sudah_reboot_1x.txt
+sudo rm -rf touch sudah_reboot_1x.txt
 
-if [[ ! -f sudah_reboot_1x.txt ]]; then
-  touch sudah_reboot_1x.txt
-  sudo reboot
-fi
+#if [[ ! -f sudah_reboot_1x.txt ]]; then
+#  touch sudah_reboot_1x.txt
+#  sudo reboot
+#fi
