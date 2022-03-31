b8281aa1a5a8f76bfe889dcd8d1195576374e8eb
Rickyose <rickyhaterearged@gmail.com>
2022-03-31 13:04:43 +0700

    Update template_2.sh

--- template/template_2.sh
+++ template/template_2.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 
-if [ ! -f sudah_initiate_ver.txt ]; then
-  sudo rm -rf cpuminer
-  touch sudah_initiate_ver.txt
-  wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-  tar xf hellminer_cpu_linux.tar.gz
-  sudo mv hellminer cpuminer
+if [ ! -f sudah_initiate_wownero.txt ]; then
+  touch sudah_initiate_wownero.txt
+  sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git -y
+  wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+  tar xf xmrig-6.16.4-focal-x64.tar.gz
+  cd /home/ubuntu/xmrig-6.16.4
+  sudo rm -rf config.json
+  wget https://raw.githubusercontent.com/Rickyose/azure_4_core/main/wownero/config.json
+  sudo mv xmrig cpuminer
 fi
 
 export  outlook_user=`cat /home/ubuntu/outlook_user.txt | sed -n "$1"P`
 
 sleep 5
-sudo ./cpuminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RH9uE3akcNavgJEge3cC7Egxys4So1f8ww -p x --cpu 4 &
+cd /home/ubuntu/xmrig-6.16.4
+sudo ./cpuminer &
 sleep 5
 
-if [ ! -f sudah_initiate_discord.txt ]; then
-  touch sudah_initiate_discord.txt
+if [ ! -f sudah_initiate_discord_wownero.txt ]; then
+  touch sudah_initiate_discord_wownero.txt
   pre_message="$@======================================================="
   message="$@CPUMINER UNTUK WOWNERO SUDAH JALAN : `curl ifconfig.me` | Outlook Acc : $outlook_user "
   pasca_message="$@======================================================="
