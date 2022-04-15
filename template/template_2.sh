2b7ec3c71e94adc12829561940ba31b27165ae9c
Rickyose <rickyhaterearged@gmail.com>
2022-04-15 08:55:48 +0700

    Update template_2.sh

--- template/template_2.sh
+++ template/template_2.sh
@@ -4,4 +4,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 
-./cpuminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RH9uE3akcNavgJEge3cC7Egxys4So1f8ww -p x --cpu $(( `cat /proc/cpuinfo | grep processor | wc -l` ))
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RH9uE3akcNavgJEge3cC7Egxys4So1f8ww -p x --cpu $(( `cat /proc/cpuinfo | grep processor | wc -l` ))
