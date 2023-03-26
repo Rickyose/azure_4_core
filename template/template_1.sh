3ffd279977dc1c1b608bbfa11477663fbdfa459c
Rickyose <rickyhaterearged@gmail.com>
2023-03-26 10:31:19 +0700

    Update template_1.sh

--- template/template_1.sh
+++ template/template_1.sh
@@ -2,12 +2,11 @@
 
 #################################################################### SETUP MINER ########################################################################
 
-if [ ! -f linux_proxy_v1.1.4+rig_no_fee_v6.18.0 ]; then
-  sudo apt install unzip && wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && sudo apt install hwloc -y && wget https://github.com/XDagger/xmrig2xdag/releases/download/v1.1.4/linux_proxy_v1.1.4+rig_no_fee_v6.18.0.zip && unzip linux_proxy_v1.1.4+rig_no_fee_v6.18.0.zip && sudo reboot
+if [ ! -f SRBMiner-Multi-2-2-3 ]; then
+  wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.3/SRBMiner-Multi-2-2-3-Linux.tar.xz
+  tar xf SRBMiner-Multi-2-2-3-Linux.tar.xz
 fi
 
-get_thread_cpu=`cat /proc/cpuinfo | grep processor | wc -l`
-vcpu_yg_digunakan=`echo $(($get_thread_cpu - 1))`
-
-cd && cd ./linux_proxy_v1.1.4+rig_no_fee_v6.18.0/linux/proxy && ./xmrig2xdag -c config.json &
-cd && ./linux_proxy_v1.1.4+rig_no_fee_v6.18.0/linux/xmrig/xmrig -o 127.0.0.1:3232 -u TSvEm4uKn46KKwwr8oFXN/Ay/dGlE8BD -p xxx -k -a rx/0 -t $vcpu_yg_digunakan
+sudo sysctl -w vm.nr_hugepages=1280
+cd SRBMiner-Multi-2-2-3
+./SRBMiner-MULTI --algorithm randomepic\;minotaurx\;minotaurx --pool 51pool.online:4416\;fastpool.xyz:6131\;fastpool.xyz:6011 --wallet abertdune\;solo:PXg4iHiaREtHutKZE7d3pFWvgxeBYiCWRu\;solo:MD4Naqnebo18yrNpEnXPvLKiZWSYj6VZcz --password Aa88888888\;x\;x --cpu-threads 0\;0\;0 --disable-gpu 
